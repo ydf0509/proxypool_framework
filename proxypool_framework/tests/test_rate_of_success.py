40ec77c1296da1ea76e6ddcb1b79064622af8995
yangdefeng <defeng.yang@skilnets.com>
2020-06-19 16:11:35 +0800

    no message

--- proxypool_framework/tests/test_rate_of_success.py
+++ proxypool_framework/tests/test_rate_of_success.py
@@ -15,35 +15,40 @@ suceess_count = 0
 total_count = 0
 total_request_time = 0
 
-for i in range(1,10000):
-    """
-
-    本项目的public代理
-    :return:
-    """
-
-    pr = json.loads(requests.get('http://127.0.0.1:6795/get_a_proxy/30', auth=('user', 'mtfy123')).text)
-
+def test(max_retry=0):
+    global  suceess_count
+    global total_count
+    global  total_request_time
 
     t_start = time.time()
-    try:
-        pr = json.loads(random.choice(REDIS_CLIENT.zrevrange('proxy_free', 0, 50)))
-        # print(pr)
-        # print(pr)
+    for j in range(0,max_retry +1):
+        pr = json.loads(requests.get('http://127.0.0.1:6795/get_a_proxy/30', auth=('user', 'mtfy123')).text)  # 接口方式
+        # pr = json.loads(random.choice(REDIS_CLIENT.zrevrange('proxy_free', 0, 50)))  # 数据库直接取
 
-        # https://ydgf.sohu.com/schedule/index.json
-        # 'http://www.baidu.com/content-search.xml'
-        # https://ydgf.sohu.com/schedule/index.json
-        resp = requests.get('https://www.baidu.com', proxies=pr, timeout=5)
-        # print(resp.text[:10])
-        logger.info(f'成功, 消耗时间 {round(time.time() - t_start, 2)}，  代理是 \033[0;41m{pr}\033[0m')
-        suceess_count += 1
-        total_request_time += time.time() - t_start
-    except Exception as e:
-        logger.warning(f'失败, 消耗时间{round(time.time() - t_start, 2)}，  代理是 \033[0;41m{pr}\033[0m')
+
+        try:
+            # print(pr)
+            # print(pr)
+            # https://ydgf.sohu.com/schedule/index.json
+            # 'http://www.baidu.com/content-search.xml'
+            # https://ydgf.sohu.com/schedule/index.json
+            resp = requests.get('https://ydgf.sohu.com/schedule/index.json', proxies=pr, timeout=5)
+            logger.info(f'重试 {j} 次请求成功, 消耗时间 {round(time.time() - t_start, 2)}，  代理是 \033[0;41m{pr}\033[0m ，结果长度是 {len(resp.text)}')
+            suceess_count += 1
+            total_request_time += time.time() - t_start
+            break
+            # print(resp.text[:10])
+        except Exception as e:
+            if j == max_retry:
+                logger.warning(f'重试了 {max_retry}次后仍然失败, 消耗时间{round(time.time() - t_start, 2)}，  代理是 \033[0;41m{pr}\033[0m，错误类型是 {type(e)}')
+
     total_count += 1
-    if i % 10 == 0 and total_count:
+    if total_count % 10 == 0 and total_count:
         logger.debug(f'当前请求总次数是 {total_count}， 成功次数是 {suceess_count} ,成功率是 {round((suceess_count / total_count) * 100, 3)}%, '
                      f'平均响应时间 {round(total_request_time / suceess_count, 2)}')
+
+
+for i in range(1,10000):
+    test(max_retry=2)
 
 
